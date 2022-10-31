7f5e103b2e71cdcdb157e6ba22a4e3b40e91ab40
mrcrashban <metelevkirill4@gmail.com>
2022-10-31 19:18:48 +0300

    small work with image button

--- app/src/main/java/com/example/financial_app/Moneyadd.java
+++ app/src/main/java/com/example/financial_app/Moneyadd.java
@@ -7,14 +7,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Moneyadd extends AppCompatActivity {
 
     private Button btnBack;
-    @SuppressLint("WrongViewCast")
+    private ImageButton back_button;
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moneyadd);
+        back_button = findViewById(R.id.imageButton2);
+        back_button.setOnClickListener(
+                v -> {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                }
+        );
+        btnBack = findViewById(R.id.button8);
+        btnBack.setOnClickListener(
+                v -> {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                }
+        );
     }
 }
